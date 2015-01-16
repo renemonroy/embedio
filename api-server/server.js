4f31bfcf2c3cc9eefee4ca3db3dfda67ca6669b0
René Monroy <rene.monroy@gmail.com>
2015-01-16 17:11:34 -0600

    Serving public

--- api-server/server.js
+++ api-server/server.js
@@ -4,9 +4,9 @@ var express = require('express'),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
   api = require('./api'),
-  port = 8080,
-  ip = '104.236.85.73',
-  // port = process.env.PORT || 3000,
+  // port = 8080,
+  // ip = '104.236.85.73',
+  port = process.env.PORT || 3000,
   server = null;
 
 mongoose.connect('mongodb://wally:Wvvd8jCsW8NxXD@ds053380.mongolab.com:53380/wall');
@@ -17,6 +17,6 @@ app
   .use(bodyParser.json())
   .use('/api', api);
 
-server = app.listen(port, ip, function() {
+server = app.listen(port, function() {
   console.log('Listening on port %d', server.address().port);
 });
