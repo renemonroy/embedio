a6ccba4f45c5647e540621e830c74f49c3aee53d
René Monroy <rene.monroy@gmail.com>
2015-01-18 01:58:00 -0600

    An update

--- app-server/app-server.js
+++ app-server/app-server.js
@@ -20,7 +20,7 @@ app
   .use(bodyParser.urlencoded({ extended : true }))
   .use(bodyParser.json())
   .use(express.static('public'))
-  .use('/assets', proxy(url.parse('http://localhost:8080/assets')))
+  .use('/assets', proxy(url.parse('http://127.0.0.1:8080/assets')))
   .use('/', webRouter)
   .use('/api', api);
 
