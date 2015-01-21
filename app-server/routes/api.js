ca841e67fde2744df686ff2b877834d31beee2f6
René Monroy <rene.monroy@gmail.com>
2015-01-21 14:32:21 -0600

    Build embeds

--- app-server/routes/api.js
+++ app-server/routes/api.js
@@ -3,6 +3,10 @@ var express = require('express'),
   Embed = require('../models/embed'),
   oembed = require('oembed-auto');
 
+var setEmbedSize = function(embedHTML, embedWidth, embedHeight) {
+  return embedHTML.replace(/(width=")\d+("\W+height=")\d+/, '$1' + embedWidth + '$2' + embedHeight);
+};
+
 module.exports = ( function() {
 
   var api = express.Router();
@@ -21,7 +25,7 @@ module.exports = ( function() {
   * Responds with an array list of all the embeds saved on the database.
   */
   api.route('/embeds').get( function(req, res) {
-    Embed.find({}).sort('-createdAt').exec(function(err, embeds) {
+    Embed.find({}).limit(3).sort('-createdAt').exec(function(err, embeds) {
       if (err) res.send(err);
       res.json({ embeds : embeds });
     });
@@ -45,14 +49,14 @@ module.exports = ( function() {
           e.image         = d.url ? d.url : '';
           e.type          = d.type;
           e.title         = d.title ? d.title : '';
-          e.width         = d.width ? d.width : 0;
-          e.height        = d.height ? d.height : 0;
-          e.html          = d.html ? d.html : '';
+          e.width         = d.width ? d.width : '100%';
+          e.height        = d.height ? d.height : '100%';
+          e.html          = d.html ? responsiveEmbed(d.html, e.width, e.height) : '';
           e.author.name   = d.author_name ? d.author_name : '';
           e.author.url    = d.author_url ? d.author_url : '';
           e.provider.name = d.provider_name;
           e.provider.url  = d.provider_url;
-          e.thumb.url     = d.thumbnail_url ? d.thumbnail_url : '';
+          e.thumb.url     = d.thumbnail_url ? d.thumbnail_url: '';
           e.thumb.width   = d.thumbnail_width ? d.thumbnail_width : 0;
           e.thumb.height  = d.thumbnail_height ? d.thumbnail_height : 0;
           e.save(function(saveError) {
